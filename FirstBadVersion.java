eeaec1f3111bda0d1c479d7e0e4de52a8632f613
Sonali Rajput <sonalirajput1088@gmail.com>
2022-02-28 18:57:15 +0530

    first bad version

---
+++ FirstBadVersion.java
@@ -0,0 +1,23 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+      public class FirstBadVersion extends VersionControl {
+        public int firstBadVersion(int n) {
+            if(n==1){
+                return 1;
+            }
+            int s = 1 , e = n;
+            int m = 0;
+            int pos = 1;
+            while(s<=e){
+                m = s + (e-s)/2;
+                if(isBadVersion(m)){
+                    pos = m;
+                    e = m-1;
+                }else{
+                    s = m+1;
+                }
+            }return pos;
+            
+        }
+    }
