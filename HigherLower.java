82e6c0eb7bf55035c093bba20ee2c2eb4c2fd052
Sonali Rajput <sonalirajput1088@gmail.com>
2022-03-01 20:15:30 +0530

    peak index in mounatain array

--- HigherLower.java
+++ HigherLower.java
@@ -1,26 +1,19 @@
-/** 
- * Forward declaration of guess API.
- * @param  num   your guess
- * @return 	     -1 if num is lower than the guess number
- *			      1 if num is higher than the guess number
- *               otherwise return 0
- * int guess(int num);
- */
 
-public class HigherLower extends GuessGame {
-    public int guessNumber(int n) {
-        int start = 1, end = n;
-        int mid =0;
-        while(start<=end){
-            mid = start + (end-start)/2;
-            int num = guess(mid);
-            if(num==0){
-                return mid;
-        }   else if(num==-1){
-                end = mid-1;
-            }else{
-                start = mid+1;
-            }
-    }return mid;
-} 
-}
+
+// public class HigherLower extends GuessGame {
+//     public int guessNumber(int n) {
+//         int start = 1, end = n;
+//         int mid =0;
+//         while(start<=end){
+//             mid = start + (end-start)/2;
+//             int num = guess(mid);
+//             if(num==0){
+//                 return mid;
+//         }   else if(num==-1){
+//                 end = mid-1;
+//             }else{
+//                 start = mid+1;
+//             }
+//     }return mid;
+// } 
+// }
