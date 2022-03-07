4016560eff17cedf4dba0de7248a230b06a52c30
Sonali Rajput <sonalirajput1088@gmail.com>
2022-03-07 19:29:07 +0530

    missing number

--- missingNumbers.java
+++ missingNumbers.java
@@ -9,6 +9,9 @@ public class missingNumbers {
         int i = 0;
         while(i<nums.length){
             int correctIndex = nums[i];
+            //we'll ignore the largest ele which will come at last cuz we don't have extra space to add it
+            //for ex : [4,0,2,1] (length = 4) and it's for N=4(so len should be 5) it should be [0,1,2,3,4] (length =5)
+            //we'll ignore it and will search for missing ele .
             if(nums[i]<nums.length&&nums[i]!=nums[correctIndex]){ //in the Nth index case the first condition will be false so it will move to else one.
                 swap(nums,i,correctIndex);
             }
@@ -18,7 +21,7 @@ public class missingNumbers {
         }
         //search for the first missing number.
         for(int index=0;index<nums.length;index++){
-            if(nums[index]!=index){
+            if(nums[index]!=index){ //if the ele is not at it's position we'll return that element's index.
                 return index;
             }
         }
