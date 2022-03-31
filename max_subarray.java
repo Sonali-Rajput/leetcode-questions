678d2b021ab62ab27998d129d5d8f5f06a45d787
Sonali Rajput <sonalirajput1088@gmail.com>
2022-03-31 23:46:15 +0530

    max in subarray

--- max_subarray.java
+++ max_subarray.java
@@ -9,10 +9,10 @@ public class max_subarray {
         public static int maxSubArray(int[] nums) {
             
             int l=nums.length;
-            int i=0,s=0,max=nums[0];
+            int i=0,s=0,max=nums[0]; 
             int[] b = new int[l];
             
-            while(i!=l)
+            while(i!=l) 
             {
                 b[i]=nums[i];
                 i++;
@@ -20,16 +20,16 @@ public class max_subarray {
             // if(l==1)
             //     return nums[0];
             Arrays.sort(b);
-            if(b[l-1]<=0)
-                return b[l-1];
-        
+            if(b[l-1]<=0) //checking if all the eements in the array are negative 
+                return b[l-1]; //then return the last
+            //else do the following ->
             i=0;
-            while(i!=l)
+            while(i!=l) 
             {
-                s+=nums[i];
-                if(s<0)
+                s+=nums[i]; //adding the next ele in the sum
+                if(s<0) //if sum is negative
                 {
-                    s=0;
+                    s=0; //the no need to add it as it will decrease the value for the sum.
                 }
                 else if(s>max)
                     max=s;
